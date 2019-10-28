9690c4ce3c29717e62f086b9b8424697ba27ead8
ardenott <ardenott@gmail.com>
2019-10-28 15:04:24 -0600

    Updated documentation, removes unneeded files.

--- acquisition.py
+++ acquisition.py
@@ -1,12 +1,17 @@
+# Arden Ott, Thomas Garza, Nate Tanner (James)
+
+import os
 import requests
 import gzip
 import pandas as pd
 
 def getcsv(url, csvname):
+    # Gzipped TSV file
     print('Downloading file... \n')
     r = requests.get(url)
     open('basics.tsv.gz', 'wb').write(r.content)
 
+    # Unzips into a TSV file
     print('Unzipping file...\n')
     basics = gzip.GzipFile('basics.tsv.gz', 'rb')
     s = basics.read()
@@ -16,9 +21,14 @@ def getcsv(url, csvname):
     output.write(s)
     output.close()
 
+    # Converts to CSV file
     print('Converting to CSV...\n')
     tsv_file = f'{csvname}.tsv'
     csv_table = pd.read_table(tsv_file, sep='\t', low_memory=False)
     csv_table.to_csv(f'{csvname}.csv', index=False,)
+    os.remove('basics.tsv.gz')
+    os.remove(f'{csvname}.tsv')
+
 
 getcsv('https://datasets.imdbws.com/title.basics.tsv.gz', 'titlebasics')
+getcsv('https://datasets.imdbws.com/title.ratings.tsv.gz', 'ratings')
