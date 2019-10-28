ac8806379d0c3f75c128a00cb4c298ee88580ab7
Arden Ott <ardenott@gmail.com>
2019-10-28 12:55:35 -0600

    Update acquisition.py
    Documenting script and including second file.

--- acquisition.py
+++ acquisition.py
@@ -1,12 +1,17 @@
+# Arden Ott, Thomas Garza, Nate Tanner (James)
+
+
 import requests
 import gzip
 import pandas as pd
 
 def getcsv(url, csvname):
+    # Gzipped TSV file
     print('Downloading file... \n')
     r = requests.get(url)
     open('basics.tsv.gz', 'wb').write(r.content)
 
+    # Unzips into a TSV file
     print('Unzipping file...\n')
     basics = gzip.GzipFile('basics.tsv.gz', 'rb')
     s = basics.read()
@@ -16,9 +21,11 @@ def getcsv(url, csvname):
     output.write(s)
     output.close()
 
+    # Converts to CSV file
     print('Converting to CSV...\n')
     tsv_file = f'{csvname}.tsv'
     csv_table = pd.read_table(tsv_file, sep='\t', low_memory=False)
     csv_table.to_csv(f'{csvname}.csv', index=False,)
 
 getcsv('https://datasets.imdbws.com/title.basics.tsv.gz', 'titlebasics')
+getcsv('https://datasets.imdbws.com/title.ratings.tsv.gz', 'rating')
