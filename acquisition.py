fad3e36ecd828e3c8dec5e55740c24a3476ed2c7
ardenott <ardenott@gmail.com>
2019-10-28 12:49:55 -0600

    get file function

--- acquisition.py
+++ acquisition.py
@@ -1,10 +1,24 @@
 import requests
 import gzip
+import pandas as pd
 
+def getcsv(url, csvname):
+    print('Downloading file... \n')
+    r = requests.get(url)
+    open('basics.tsv.gz', 'wb').write(r.content)
 
-with gzip.open(requests.get('https://datasets.imdbws.com/title.basics.tsv.gz')) as f:
-    titles_content = f.read()
+    print('Unzipping file...\n')
+    basics = gzip.GzipFile('basics.tsv.gz', 'rb')
+    s = basics.read()
+    basics.close()
 
+    output = open(f'{csvname}.tsv', 'wb')
+    output.write(s)
+    output.close()
 
-with gzip.open(requests.get('https://datasets.imdbws.com/title.ratings.tsv.gz')) as f:
-    ratings_content = f.read()
+    print('Converting to CSV...\n')
+    tsv_file = f'{csvname}.tsv'
+    csv_table = pd.read_table(tsv_file, sep='\t', low_memory=False)
+    csv_table.to_csv(f'{csvname}.csv', index=False,)
+
+getcsv('https://datasets.imdbws.com/title.basics.tsv.gz', 'titlebasics')
