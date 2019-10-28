a64a3a42377ace977ec3e0565df3b7554182c134
ntanner <natetann1@gmail.com>
2019-10-28 11:59:50 -0600

    Downloading files

--- acquisition.py
+++ acquisition.py
@@ -1,5 +1,12 @@
 import requests
+import gzip
 
-#test commit
 
-#test again
+with gzip.open(requests.get('https://datasets.imdbws.com/title.basics.tsv.gz')) as f:
+    titles_content = f.read()
+
+
+with gzip.open(requests.get('https://datasets.imdbws.com/title.ratings.tsv.gz')) as f:
+    ratings_content = f.read()
+
+
