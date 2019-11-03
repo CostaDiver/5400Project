897556a15a010e417c8243c9632c9ba9c7bff6d9
Arden Ott <ardenott@gmail.com>
2019-11-03 15:05:03 -0700

    Update acquisition.py
    Removing non-movie lines

--- acquisition.py
+++ acquisition.py
@@ -14,10 +14,11 @@ import os
 import requests
 import gzip
 import pandas as pd
+import csv
 
 
 def getcsv(url, csvname):
-    # Gzipped TSV file
+    # Gets Gzipped TSV file
     print('Downloading file... \n')
     r = requests.get(url)
     open('basics.tsv.gz', 'wb').write(r.content)
@@ -37,10 +38,40 @@ def getcsv(url, csvname):
     csv_table = pd.read_table(tsv_file, sep='\t', low_memory=False)
     csv_table.to_csv(f'{csvname}.csv', index=False,)
 
-    # Close unneeded files
+    # Remove all rows except for movies
+    print('Deleting non-movie lines...')
+    inp = open(f'{csvname}.csv', 'r')
+    out = open(f'{csvname}_edit.csv', 'w')
+    writer = csv.writer(out)
+
+    if csvname == 'titlebasics':
+        first_line = True
+        for row in csv.reader(inp):
+            if first_line:
+                first_line = False
+                writer.writerow(row)
+            elif row[1] == 'movie':
+                writer.writerow(row)
+    elif csvname == 'ratings':
+        titlebasics = open('titlebasics_edit.csv')
+        tconst = set()
+        for row in csv.reader(titlebasics):
+            tconst.add(row[0])
+        first_line = True
+        for row in csv.reader(inp):
+            if first_line:
+                first_line = False
+                writer.writerow(row)
+            elif row[0] in tconst:
+                writer.writerow(row)
+
+    # Delete unneeded files
     os.remove('basics.tsv.gz')
     os.remove(f'{csvname}.tsv')
+    os.remove(f'{csvname}.csv')
 
 
+# Basics file contains all movies and basic info like title, year released, etc.
 getcsv('https://datasets.imdbws.com/title.basics.tsv.gz', 'titlebasics')
+# Contains ratings for all movies.
 getcsv('https://datasets.imdbws.com/title.ratings.tsv.gz', 'ratings')
