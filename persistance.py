2fc4f356fa9699a12b117ba74f919734713947fb
ntanner <natetann1@gmail.com>
2019-11-03 13:18:24 -0700

    Answered persistence assignment questions.

---
+++ persistance.py
@@ -0,0 +1,33 @@
+'''
+Answer the Following Questions:
+
+1) Which database will you be using for you final project? (e.g. SQL Server, MongoDB, Other...) (5pts)
+2) Where will the database you are using be hosted? (e.g. Azure, Locally, Shared MIS Dept.) (5pts)
+3) Why did you choose the database and hosting option you did? (10 pts)
+
+
+
+Do the Following:
+1) Setup the database you will be using. (10 pts.)
+2) Connect to the database with a python script and upload the script. If the script contains sensitive information
+ such as credentials that cannot be shared, please take a screen-shot of the connection and blot out sensitive
+  information. (10 pts.)
+3) Describe the schema you will use in your database. If it is a relational database (e.g. SQL Server), share your
+ table schema, if it is a document store (e.g. MongoDB) then share a sample JSON file that represents the data
+  you intend to store. (10 pts)
+
+
+
+NOTE: If you are working in a group, please list the group member names and have each member upload individually.
+'''
+
+'''
+1) We will be using an Azure SQL Server Database, imdbdata.database.windows.com
+2) See question 1.
+3) We chose it because it allowed all of us to have good accessibility to the DB, learn something new, and familiarize
+    ourselves with cloud hosted database services. We chose SQL Server because its what we use in the Hunstman School
+     database courses so we all have familiarity with it.
+     
+ 1) The database is set up on Azure at imdbdata.databasecom
+ 2) See below script.
+'''
