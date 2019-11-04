a5fda81f1d071d0da327b0e031c5a441bd034636
Arden Ott <ardenott@gmail.com>
2019-11-04 10:17:53 -0700

    Update acquisition.py
    Made a number of changes to reduce memory usage and improve performance on low-end machines.

--- acquisition.py
+++ acquisition.py
@@ -1,83 +1,90 @@
-# Arden Ott, Thomas Garza, Nate (James) Tanner
-#
-# A small function that is designed to take a IMDB data URL, download, unzip, and convert it to CSV.
-# One file contains every single item on IMDB, and basic info about it. The other file contains the user ratings for
-# all of those items. The data is quite large, combined it works out to a little over half a GB. Part of this issue
-# is that we only need movies, but the data includes short films, video games, and other non-movie datapoints. Stripping
-# those datapoints will be done in the next part of the project.
-#
-# The data seems pretty clean, so it doesn't look like we will need to do any major scrubbing aside from filtering
-# out unneeded lines.
+"""
+Arden Ott, Thomas Garza, Nate (James) Tanner
+
+A small function that is designed to take a IMDB data URL, download, unzip, and convert it to CSV.
+One file contains every single item on IMDB, and basic info about it. The other file contains the user ratings for
+all of those items. The data is quite large, combined it works out to a little over half a GB. Part of this issue
+is that we only need movies, but the data includes short films, video games, and other non-movie datapoints.
+
+The data seems pretty clean, so it doesn't look like we will need to do any major scrubbing aside from filtering
+out unneeded lines and including NULL values.
+"""
 
 
-import os
-import requests
-import gzip
-import pandas as pd
-import csv
+from os import remove
+from requests import get
+from gzip import GzipFile
+from pandas import read_table
+from csv import reader, writer
 
 
 def getcsv(url, csvname):
     # Gets Gzipped TSV file
     print('Downloading file... \n')
-    r = requests.get(url)
+    r = get(url)
     open('basics.tsv.gz', 'wb').write(r.content)
 
     # Unzips into a TSV file
     print('Unzipping file...\n')
-    basics = gzip.GzipFile('basics.tsv.gz', 'rb')
+    basics = GzipFile('basics.tsv.gz', 'rb')
     s = basics.read()
     basics.close()
     output = open(f'{csvname}.tsv', 'wb')
     output.write(s)
     output.close()
-
+    datatypes = dict()
+    # Define column datatypes
+    if csvname == 'titlebasics':
+        datatypes = {'tconst': str, 'titleType': str, 'primaryTitle': str, 'originalTitle': str, 'isAdult': str,
+                     'startYear': str, 'endYear': str, 'runtimeMinutes': str, 'genres': str}
+    elif csvname == 'ratings':
+        datatypes = {'tconst': str, 'averageRating': float, 'numVotes': int}
     # Converts to CSV file
     print('Converting to CSV...\n')
     tsv_file = f'{csvname}.tsv'
-    csv_table = pd.read_table(tsv_file, sep='\t', low_memory=False)
+    csv_table = read_table(tsv_file, sep='\t', dtype=datatypes)
     csv_table.to_csv(f'{csvname}.csv', index=False,)
 
     # Remove all rows except for movies
-    print('Deleting non-movie lines...')
+    print('Deleting non-movie lines...\n')
     inp = open(f'{csvname}.csv', 'r')
     out = open(f'{csvname}_edit.csv', 'w')
-    writer = csv.writer(out)
+    csvwriter = writer(out)
 
-    if csvname == 'titlebasics':
+    if csvname == 'titlebasics':  # This only runs on the titlebasics dataset
         first_line = True
-        for row in csv.reader(inp):
+        for row in reader(inp):
             while '\\N' in row:  # Replace \N values with nothing
                 location = row.index('\\N')
                 row[location] = ''
             if first_line:  # Always write first line
                 first_line = False
-                writer.writerow(row)
+                csvwriter.writerow(row)
             elif row[1] == 'movie':  # Write lines that include movie as the title type
-                writer.writerow(row)
+                csvwriter.writerow(row)
 
-    elif csvname == 'ratings':
-        # Open TitleBasics and write all Tconst values to a set
+    elif csvname == 'ratings':  # This only runs on the ratings dataset
+        # Open TitleBasics and write all Tconst values to a set so that it can be easily searched
         titlebasics = open('titlebasics_edit.csv')
         tconst = set()
-        for row in csv.reader(titlebasics):
+        for row in reader(titlebasics):
             tconst.add(row[0])
 
         first_line = True
-        for row in csv.reader(inp):
+        for row in reader(inp):
             while '\\N' in row:  # Replace \N values with nothing
                 location = row.index('\\N')
                 row[location] = ''
             if first_line:  # Always write first line
                 first_line = False
-                writer.writerow(row)
+                csvwriter.writerow(row)
             elif row[0] in tconst:  # Only write lines that have a matching tconst
-                writer.writerow(row)
+                csvwriter.writerow(row)
 
     # Delete unneeded files
-    os.remove('basics.tsv.gz')
-    os.remove(f'{csvname}.tsv')
-    os.remove(f'{csvname}.csv')
+    remove('basics.tsv.gz')
+    remove(f'{csvname}.tsv')
+    remove(f'{csvname}.csv')
 
 
 # Basics file contains all movies and basic info like title, year released, etc.
