e013043886cfad9fa19c575b69a70016bb6078a4
ardenott <ardenott@gmail.com>
2019-11-10 21:03:38 -0700

    Added comments, added HTML to format welcome page, formatted SQL, added doctext at beginning of file.

--- access.py
+++ access.py
@@ -1,15 +1,14 @@
+"""
+Arden Ott, Thomas Garza, Nate (James) Tanner
+
+A Python script which creates a connection to our database from which we persisted data in the last assignment. Once
+connected, we create a Flask app to allow the database to be accessed and edited over the internet. Data is returned
+and entered in the JSON format. Movie lookups and row deletions are done using the tconst key value.
+"""
+
 from flask import Flask, request
 import pyodbc
-import sqlalchemy
-from sqlalchemy.sql import sqltypes as types
 
-"""
-Default Route ("/") - Go to a simple html template page that tells about your data.
-GET ("/item") - Will return UP TO 1000 items from your data.
-GET ("/item/<id>") - Will return a single item from your data, by ID. If you data does not have a unique identifier then please let me know and I will help you get one added.
-DELETE("/item/<id>") - Will delete a single item (again, you will need a unique column name)
-POST ("/item") - As opposed to GET, POST will create a new item in your database. The body of the request will contain the item to be added. 
-"""
 
 # Connect to server
 server = 'imdbdata.database.windows.net'
@@ -20,50 +19,67 @@ driver = '{ODBC Driver 17 for SQL Server}'
 
 conn = pyodbc.connect('DRIVER=' + driver + f';SERVER={server};DATABASE={database};UID={username};PWD={password}')
 cursor = conn.cursor()
+
 # Create Flask app
 app = Flask(__name__)
 
-
 # Routes
-@app.route('/')
+@app.route('/')  # Welcome screen describing the API
 def welcome():
-    desc = '''A small function that is designed to take a IMDB data URL, download, unzip, and convert it to CSV.'
-desOne file contains every single item on IMDB, and basic info about it. The other file contains the user ratings for
-all of those items. The data is quite large, combined it works out to a little over half a GB. Part of this issue
-is that we only need movies, but the data includes short films, video games, and other non-movie datapoints. Stripping
-the file of those rows shrinks it to about 45 MB.'''
+    desc = '''<br><h3>A small Flask app to return IMDB movie data. Available routes can be found below: </h3>
+              <ul>
+              <li>/item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Returns the top 1000 movies sorted by release year and rating. Can be used as a get or post 
+                (using JSON) function.
+                </li>
+              <li>/item/id&nbsp;&nbsp;&nbsp;
+                Returns a specific title where id is the tconst of the film. Can be used as a get or delete function.
+                </li>
+              </ul>'''
     return desc
 
 
-@app.route('/item', methods=['GET'])
+@app.route('/item', methods=['GET'])  # Gets the top 1000 rows of our data sorted by year and rating descending
 def return_1000():
-    records = cursor.execute('''SELECT TOP 1000 Titles.tconst, primaryTitle, startYear, averageRating, numVotes
+    records = cursor.execute('''
+    SELECT TOP 1000 Titles.tconst
+                    ,primaryTitle
+                    ,startYear
+                    ,averageRating
+                    ,numVotes
     FROM Ratings
-    JOIN Titles
-    ON Ratings.tconst = Titles.tconst
+        JOIN Titles
+        ON Ratings.tconst = Titles.tconst
     WHERE isAdult = 0
-    ORDER BY startYear DESC, averageRating desc
+    ORDER BY startYear DESC
+             ,averageRating DESC
     FOR JSON AUTO''').fetchone()
 
     cursor.close()
     return records[0]
 
 
-@app.route('/item/<id>', methods=['GET'])
+@app.route('/item/<id>', methods=['GET'])  # Gets a specific movie by searching the tconst
 def return_one(id):
-    records = cursor.execute('''SELECT Titles.tconst, primaryTitle, startYear, averageRating, numVotes
+    records = cursor.execute('''
+    SELECT Titles.tconst
+           ,primaryTitle
+           ,startYear
+           ,averageRating
+           ,numVotes
     FROM Ratings
-    JOIN Titles
-    ON Ratings.tconst = Titles.tconst
+        JOIN Titles
+        ON Ratings.tconst = Titles.tconst
     WHERE isAdult = 0 AND Titles.tconst = (?)
-    ORDER BY startYear DESC, averageRating desc
+    ORDER BY startYear DESC
+             ,averageRating DESC
     FOR JSON AUTO;''', id).fetchone()
 
     cursor.close()
     return records[0]
 
 
-@app.route('/item/<id>', methods=['DELETE'])
+@app.route('/item/<id>', methods=['DELETE'])  # Allows for the deleting of a movie by entering the tconst
 def delete_one(id):
     try:
         cursor.execute('''DELETE FROM Ratings WHERE Ratings.tconst = (?);''', id)
@@ -74,10 +90,31 @@ def delete_one(id):
         return 'Delete Failed.'
 
 
-@app.route('/item', methods=['POST'])
+@app.route('/item', methods=['POST'])  # Allows a new row to be entered with data in JSON format
 def post_one():
+    """
+    To test this function, use this JSON data as the input:
+
+    {
+        "tconst":"tt10914342",
+        "titleType":"movie",
+        "primaryTitle":"Kirket",
+        "originalTitle":"Kirket",
+        "isAdult":"0",
+        "startYear":2019,
+        "endYear":"",
+        "runtimeMinutes":"132",
+        "genres":"Drama,Sport",
+        "Ratings":
+            [{"averageRating":10,
+            "numVotes":580}]
+    }
+
+    """
+
     data = request.get_json()
 
+    # Declare column values
     tconst = data['tconst']
     titleType = data['titleType']
     primaryTitle = data['primaryTitle']
@@ -93,6 +130,7 @@ def post_one():
     query_titles = 'INSERT INTO Titles VALUES(?,?,?,?,?,?,?,?,?)'
     query_ratings = 'INSERT INTO Ratings VALUES(?, ?, ?)'
 
+    # Don't run if the tconst is already found in the database
     if len(cursor.execute('SELECT COUNT(*) FROM Titles WHERE Titles.tconst = (?)', tconst).fetchall()) > 0:
         return f'Record with tconst {tconst} already exists in DB'
 
@@ -104,16 +142,8 @@ def post_one():
         conn.commit()
         return 'Record inserted.'
     except:
-        return 'Not inserted'
+        return 'Record not inserted.'
 
 
 if __name__ == '__main__':
     app.run()
-
-# [{"tconst":"tt10914342","titleType":"movie","primaryTitle":"Kirket","originalTitle":"Kirket","isAdult":"0","startYear":2019, "endYear":"","runtimeMinutes":"132","genres":"Drama,Sport","Ratings":[{"averageRating":10,"numVotes":580}]}]
-
-# tconst	titleType	primaryTitle	originalTitle	isAdult	startYear	endYear	runtimeMinutes	genres
-# tt10914342	movie	Kirket	Kirket	0	2019		132	Drama,Sport
-
-# tconst	averageRating	numVotes
-# tt10914342	10	583
