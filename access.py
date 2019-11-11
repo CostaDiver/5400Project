3ebfd63ac5b24d136af614a8cf023a7310d23d43
ntanner <natetann1@gmail.com>
2019-11-10 18:25:05 -0700

    Added assignment desc

--- access.py
+++ access.py
@@ -3,6 +3,14 @@ import pyodbc
 import sqlalchemy
 from sqlalchemy.sql import sqltypes as types
 
+"""
+Default Route ("/") - Go to a simple html template page that tells about your data.
+GET ("/item") - Will return UP TO 1000 items from your data.
+GET ("/item/<id>") - Will return a single item from your data, by ID. If you data does not have a unique identifier then please let me know and I will help you get one added.
+DELETE("/item/<id>") - Will delete a single item (again, you will need a unique column name)
+POST ("/item") - As opposed to GET, POST will create a new item in your database. The body of the request will contain the item to be added. 
+"""
+
 # Connect to server
 server = 'imdbdata.database.windows.net'
 database = 'imdbdata'
